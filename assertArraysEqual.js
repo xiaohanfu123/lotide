bd4e02a09c61aa01ebc953a922828499eb602c05
xiaohanfu123 <shawnx1995@gmail.com>
2022-08-24 17:11:33 +0000

    first commit of asserArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,30 +1,23 @@
-// const eqArrays = function(array1,array2) {
 
-//   let intString = array1.join('');
-//   for (let i = 0; i < array2.length; i++) {
-//     const words = array2.join('');
-//     if(intString !== words){return false  
-//     }
-//   }
-//     return true
-//   }
- 
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     return console.log(`✅✅Assertion Passed:${actual} === ${expected}`);
-//   }
-//   console.assert(actual === expected,`⛔⛔${actual} !== ${expected}`);
-// };
-
-// assertEqual((eqArrays([1,2,3],[1,2,3])) , true);
+const eqArrays = function(array1,array2) {
+  if (array1.length !== array2.length) { 
+    return false
+  }
+  for (let i = 0; i < array2.length; i++) {
+    if(array1[i] !== array2[i]){return false  
+    } 
+  } 
+    return true
+  } 
 
 
-const assertArraysEqual = (array1, array2) =>{
-    let intString = array1.join('');
-    for (let i = 0; i < array2.length; i++) {
-      const words = array2.join('');
-      if(intString !== words){return false  
-      }
-    }
-      return true
+  const assertArraysEqual = (array1,array2) => {
+    if (eqArrays(array1,array2) == 1) {
+      return console.log(`✅✅Assertion Passed:${array1} === ${array2}`);
+  }
+  else
+  console.assert(eqArrays(array1,array2) == 1,`⛔⛔${array1} !== ${array2}`);
 }
+
+assertArraysEqual([1,2,3],[1,2,3])
+
