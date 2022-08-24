036bb17ba050851a5936c585bb0c54ddf3e14cf7
xiaohanfu123 <shawnx1995@gmail.com>
2022-08-24 19:19:12 +0000

    finally

---
+++ middle.js
@@ -0,0 +1,39 @@
+
+const eqArrays = function(array1,array2) {
+  if (array1.length !== array2.length) { 
+    return false
+  }
+  for (let i = 0; i < array2.length; i++) {
+    if(array1[i] !== array2[i]){return false  
+    } 
+  } 
+    return true
+  } 
+
+
+  const assertArraysEqual = (array1,array2) => {
+    if (eqArrays(array1,array2) == 1) {
+      return console.log(`✅✅Assertion Passed:${array1} === ${array2}`);
+  }
+  else
+  console.assert(eqArrays(array1,array2) == 1,`⛔⛔${array1} !== ${array2}`);
+}
+
+// assertArraysEqual([1,2,3],[1,2,3])
+
+const middle = (array) => {
+  let newArray = []
+  let length = array.length
+
+  if (array.length <= 2){
+    return newArray}
+  
+  if (array.length % 2 == 0) { 
+    newArray.push(array[length / 2 -1]  , array[length / 2 ] )
+    return newArray
+  } 
+   newArray.push(array[Math.floor(length /2)])
+   return newArray
+}
+
+console.log(middle([1]))
