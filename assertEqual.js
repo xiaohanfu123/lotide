25937995f85ae6543a74dca55b6de81d62859c06
xiaohanfu123 <shawnx1995@gmail.com>
2022-08-25 01:10:09 +0000

    save

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return console.log(`✅✅Assertion Passed:${actual} === ${expected}`);
+    return console.log(`ssertion Passed:✅✅${actual} === ${expected}`);
   }
   console.assert(actual === expected,`⛔⛔${actual} !== ${expected}`);
 };
