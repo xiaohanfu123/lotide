d096fcb187cf6153b408d0bb9ae7f0632bc7e83f
xiaohanfu123 <shawnx1995@gmail.com>
2022-08-27 05:40:37 +0000

    it can run

--- findKey.js
+++ findKey.js
@@ -1,30 +1,32 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     return console.log(`ssertion Passed:✅✅${actual} === ${expected}`);
   }
-  console.assert(actual === expected,`⛔⛔${actual} !== ${expected}`);
+  console.assert(actual === expected, `⛔⛔${actual} !== ${expected}`);
 };
 
-
-
-
-const findKey = (object,key) =>{
-  for(let genre in object){
-    if(object[genre] == key)
-    return genre// I am not fully clear why it passed, because it passed a key instead of string ) Hold on .... alll keys are strings!
-    
+const findKey = (object, callback)=>{
+  for (let key in object) {
+    if(callback(object[key])){
+      return key
+    }
   }
 }
 
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2) )
 
-
-const bestTVShowsByGenre = { 
-  'sci_fi': "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-
-
-assertEqual(findKey(bestTVShowsByGenre, "The Expanse"), "drama");
-assertEqual(findKey(bestTVShowsByGenre, "The Wire"), 'drama');
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 3), 'Akaleri')
